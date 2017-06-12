8f1af2cb6812342df7e4e71b79a6d10cef1384ad
Naveena Naganaboina <smilingnavee@gmail.com>
2017-06-12 11:56:26 +0530

    Create Triangular_pyramid_sum.cpp

--- Triangular_pyramid_sum.cpp
+++ Triangular_pyramid_sum.cpp
@@ -1,5 +1,12 @@
 // Triangular_sum.cpp : Defines the entry point for the console application.
-//
+/*By starting at the top of the triangle below and moving to adjacent numbers on the row below, 
+the maximum total from top to bottom is 23. That is, 3 + 7 + 4 + 9 = 23.
+
+3
+7 4
+2 4 6
+8 5 9 3
+*/
 
 #include <iostream>
 #include <sstream>
